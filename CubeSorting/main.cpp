9f01e5a1707c0b8423b49082eec7a8caf281b2ec
Henry Jackson <Henry@Henrys-MacBook-Pro.local>
2015-09-28 18:52:26 +0100

    Initial Commit

---
+++ CubeSorting/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CubeSorting
+//
+//  Created by Henry Jackson on 28/09/2015.
+//  Copyright (c) 2015 Henry Jackson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
